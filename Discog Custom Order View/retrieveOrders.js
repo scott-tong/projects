c4621c64a4ff93291c8b0d487198fa4b8a189786
scott-tong <scott.t.tong@gmail.com>
2018-01-23 21:18:10 -0500

    Display an order and list of orders

--- Discog Custom Order View/retrieveOrders.js
+++ Discog Custom Order View/retrieveOrders.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
-	displayOrder();
+	displayOrderList();
 });
 
 //display orders
-function displayOrder(){
-	$.getJSON("orderListExample.json", function(data){
+function displayOrderList(){
+	// $.getJSON("orderListExample.json", function(data){
+	$.getJSON("https://api.discogs.com/marketplace/orders?status=New Order&token=pMpLSjtRlBohrZjOIMgrKhDSkZaqlpcPJnZdQMys", function(data){
 		var items = [];
 		$.each(data["orders"], function(key,value){
-			items.push( "<div id='orderID'>Order Number: " + value['items'].length + "</div>" );
-			items.push( "<div id='numItems'>Number of Items : " + value['id'] + "</div>" );
-			items.push( "<div id='lastUpdated'>Last updated : " + value['last_activity'] + "</div>" );
+			items.push("<div id='orderID'>Order Number: " + value['id'] + "</div>" );
+			items.push("<a id='orderID' href=https://api.discogs.com/marketplace/orders/" + value['id'] + "?token=pMpLSjtRlBohrZjOIMgrKhDSkZaqlpcPJnZdQMys>" + "JSON Link</a>");
+			items.push("<br/>")
+			items.push("<a id='orderID' href=discogDisplayOrder.html?orderID=" + value['id'] + ">Page link</a>");
+			items.push("<div id='numItems'>Number of Items : " + value['items'].length + "</div>" );
+			items.push("<div id='lastUpdated'>Last updated : " + value['last_activity'] + "</div>" );
+			items.push("<div id='status'>Status : " + value['status'] + "</div>" );
+			items.push("<div id='shippingAddress'>Shipping Address: "+ value['shipping_address'] + "</div>")
+			items.push("<br/>")
 		});
 
 		$("<div/>", {
