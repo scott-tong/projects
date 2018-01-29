005aa330e9cbb6a6d6bd1a42fd6668ea74a5f9e2
scott-tong <scott.t.tong@gmail.com>
2018-01-28 20:51:19 -0500

    Update

---
+++ Discog Custom Order View/releaseRetrieve-standalone.js
@@ -0,0 +1,73 @@
+var listIndex = 0;
+var orderJson;
+var oauth_consumer_key = "BwMnLoVlnyQMZHNbCEUc";
+var oauth_token = "qVsUidCcOBTNxYOsgeRzrwNHBepCpCHCeRtbYdKv";
+var token = "JWFLkaHOvPVGJzKKgXCpdxaTprBxmNqFdkJWHfWJ";
+
+$(document).ready(function() {	
+	// Activates knockout.js
+	ko.applyBindings(new discogReleaseViewModel());
+});
+
+function nextRelease(){
+	listIndex += 1;
+	if(listIndex > orderJson["items"].length-1){
+		listIndex = 0;
+	}
+	$("#releaseInfo").html('');
+	getAndDisplayRelease(getReleaseIdFromOrder(orderJson, listIndex));
+}
+
+function previousRelease(){
+	listIndex -= 1;
+	if(listIndex <  0){
+		listIndex = orderJson["items"].length-1;
+	}
+	$("#releaseInfo").html('');
+	getAndDisplayRelease(getReleaseIdFromOrder(orderJson, listIndex));
+}
+
+function getReleaseIdFromOrder(data, index){
+	return data["items"][index]["release"]["id"];
+}
+
+function getAndDisplayRelease(releaseId){
+	//TODO: implement throttling of requests by reading response headers X-discog-RateLimit-Remaining. If it is 0, throttle request and notify user
+
+   //$.getJSON( "test2.json", function( data ) {
+   $.getJSON( "https://api.discogs.com/releases/"+releaseId+"?token="+token, function( data ) {
+   //$.getJSON( "https://api.discogs.com/releases/564564564", function( data ) {
+	var items = [];
+	console.log(JSON.stringify(data));
+	console.log('Sucessful json return');
+	$.each(data['artists'], function(key, value){
+		items.push( "<div id='artist'>" + value['name'] + "</div>" );
+	});
+	items.push( "<div id='title'>" + data['title'] + "</div>" );
+	items.push( "<div id='year'>" + data['year'] + "</div>" );
+
+	if(data['thumb'] == ''){
+		items.push( "<img id='thumb' src='vinyl-detailed-illustration-black-record-blank-cover-case-46399114.jpg'>" );
+	}
+	else{
+		items.push( "<img id='thumb' src='" + data['thumb'] + "'>" );
+	}
+		
+	$("<div/>", {
+		html: items.join( "" )
+		}).appendTo( "#releaseInfo" );
+	})
+  .fail(function() {
+  	$("#releaseInfo").append("<div id='fail'> failed </div>");
+  });
+}
+
+function discogReleaseViewModel(){
+	this.releaseIdToLookup = ko.observable("");
+
+	this.lookUpRelease = function(){
+	$("#releaseInfo").html('');
+		getAndDisplayRelease(this.releaseIdToLookup());
+	};
+
+}
---
+++ Discog Custom Order View/retrieveOrders.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	displayOrderList();
+});
+
+//display orders
+function displayOrderList(){
+	// $.getJSON("orderListExample.json", function(data){
+	$.getJSON("https://api.discogs.com/marketplace/orders?status=New Order&token=", function(data){
+		var items = [];
+		$.each(data["orders"], function(key,value){
+			items.push("<div id='orderID'>Order Number: " + value['id'] + "</div>" );
+			items.push("<a id='orderID' href=https://api.discogs.com/marketplace/orders/" + value['id'] + "?token=>" + "JSON Link</a>");
+			items.push("<br/>")
+			items.push("<a id='orderID' href=discogDisplayOrder.html?orderID=" + value['id'] + ">Page link</a>");
+			items.push("<div id='numItems'>Number of Items : " + value['items'].length + "</div>" );
+			items.push("<div id='lastUpdated'>Last updated : " + value['last_activity'] + "</div>" );
+			items.push("<div id='status'>Status : " + value['status'] + "</div>" );
+			items.push("<div id='shippingAddress'>Shipping Address: "+ value['shipping_address'] + "</div>")
+			items.push("<br/>")
+		});
+
+		$("<div/>", {
+			html: items.join( "" )
+		}).appendTo( "#orderList" );
+	})
+  	.fail(function() {
+  		$("#orderList").append("<div id='fail'> failed </div>");
+  	});
+}
+
+
+//retrieve orders
